f4385382597e5288a8a8bbbbd04aab574dd60c1a
AnshumanPadhi-3103 <42178537+AnshumanPadhi-3103@users.noreply.github.com>
2018-08-14 15:58:00 +0530

    Add files via upload
    Fun doing it

---
+++ Anshuman.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "what is your name?"
+read x
+mkdir $x
+cd $x
+touch $x.cpp
+#!bin/bash
+echo "what is your name?"
+read x
+mkdir $x
+cd $x
+for i in 1 2 3 4 5 
+   do
+     touch $x$i.cpp
+   done
