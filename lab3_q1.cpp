25529120453379ca2436e5b6ae39e519b676b42a
AnshumanPadhi-3103 <42178537+AnshumanPadhi-3103@users.noreply.github.com>
2018-08-16 20:47:12 +0530

    Add files via upload

---
+++ lab3_q1.cpp
@@ -0,0 +1,32 @@
+//Library
+#include <iostream>
+//using namespace keyword
+using namespace std;
+//writing main function
+int main()
+{
+//declaring variables of all types
+int one;
+float two;
+double three;
+char four;
+bool five;
+one=1;
+two=2.22;
+three=3;
+four='4';
+five=true;
+//output of values
+cout<<one<<endl;
+cout<<two<<endl;
+cout<<three<<endl;
+cout<<four<<endl;
+cout<<five<<endl;
+//size
+cout<<"size of one="<<sizeof(one)<<endl;
+cout<<"size of two="<<sizeof(two)<<endl;
+cout<<"size of three="<<sizeof(three)<<endl;
+cout<<"size of four="<<sizeof(four)<<endl;
+cout<<"size of five="<<sizeof(five)<<endl;
+return 0;
+}
---
+++ lab3_q2.cpp
@@ -0,0 +1,21 @@
+//Library
+#include<iostream>
+using namespace std;
+int main()
+{
+//declaring variables
+float a,b;
+//Initialising variables
+a=12;
+b=48;
+cout<<"the first variables are "<<a<<" and "<<b<<endl;
+//displaying the functions
+cout<<"a+b="<<a+b<<endl;
+cout<<"a-b="<<a-b<<endl;
+cout<<"a*b="<<a*b<<endl;
+cout<<"b/a="<<b/a<<endl;
+int c=1,d=69;
+cout<<"the mod is "<<c%d<<endl;
+return 0;
+}
+
